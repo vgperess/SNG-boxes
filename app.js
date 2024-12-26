61ce4f1f80d781b8721a8cac2b8a3632d8158283
Victor Garcia <167478968+vgperess@users.noreply.github.com>
2024-12-26 14:18:18 -0300

    code was full english translated

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
 alert("Welcome to the Secret Number Game!!");
-let numeroSecreto = parseInt(Math.random() * 10 + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+let secretNumber = parseInt(Math.random() * 10 + 1);
+console.log(secretNumber);
+let guess;
+let tries = 1;
 
-while(chute != numeroSecreto)
+while(guess != secretNumber)
 {
-    chute = prompt(`Choose a number between 1 and 10`);
-    if (chute == numeroSecreto) 
+    guess = prompt(`Choose a number between 1 and 10`);
+    if (guess == secretNumber) 
         {
         break;
         }
         
         else
         {
-            if(numeroSecreto > chute)
+            if(secretNumber > guess)
             {
-                alert(`The number is higher than ${chute}!`);
+                alert(`The number is higher than ${guess}!`);
             } 
         
-            if(numeroSecreto < chute)
+            if(secretNumber < guess)
             {
-                alert(`The number is lower than ${chute}!`);
+                alert(`The number is lower than ${guess}!`);
             }
 
-            tentativas++;
+            tries++;
         }    
 }
 
-let palavraTentativa = tentativas > 1 ? "tries" : "try";
-alert(`Nice!! You got the number ${numeroSecreto} with ${tentativas} ${palavraTentativa}.`);
-
-// if(tentativas > 1)
-//{
-//    alert(`Boa!! Você descobriu o número secreto!! O número era ${numeroSecreto} com ${tentativas} tentativas.  `)
-//}
-//else
-//{
-//   alert(`Boa!! Você descobriu o número secreto!! O número era ${numeroSecreto} com ${tentativas} tentativa.  `)
-//}
+let wordTry = tries > 1 ? "tries" : "try";
+alert(`Nice!! You got the number ${secretNumber} with ${tries} ${wordTry}.`);
